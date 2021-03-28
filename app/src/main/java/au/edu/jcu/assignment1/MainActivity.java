5553f1572988830f94d3bb6463bc6d0c32ccb543
Alex <alexander.campbell1@my.jcu.edu.au>
2021-03-28 20:40:37 +1000

    fix position1 is null error fix misinformation in Converter.java and Unit.java add data to AndroidManifest.xml

--- app/src/main/java/au/edu/jcu/assignment1/MainActivity.java
+++ app/src/main/java/au/edu/jcu/assignment1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +15,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String POS1 = "position1";
+    private static final String POS2 = "position2";
     private String position1;
     private String position2;
 
@@ -21,32 +24,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button cbutton;
-        Button sbutton;
-        final EditText measurement;
+        Button covertButton;
+        Button settingsButton;
+        final TextView to_convert;
+        final TextView from_convert;
+        final EditText measurementInput;
         final TextView textView;
 
-        sbutton = (Button) findViewById(R.id.settingsButton);
-        cbutton = (Button) findViewById(R.id.convertButton);
-        measurement = (EditText) findViewById(R.id.measurement);
+        to_convert = (TextView) findViewById(R.id.to_convert);
+        from_convert = (TextView) findViewById(R.id.from_convert);
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        covertButton = (Button) findViewById(R.id.convertButton);
+        measurementInput = (EditText) findViewById(R.id.measurement);
         textView = (TextView) findViewById(R.id.textView);
 
-        cbutton.setOnClickListener(new View.OnClickListener() {
+        if (savedInstanceState != null) {
+            position1 = savedInstanceState.getString(POS1);
+            position2 = savedInstanceState.getString(POS2);
+            from_convert.setText(position2);
+            to_convert.setText(position1);
+        }
+        else {
+            position1 = "Inch";
+            position2 = "Inch";
+        }
+
+        covertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (measurementInput.getText().toString().isEmpty()) {
+                    textView.setError("Please enter a number");
+                } else {
+                    double input = Double.parseDouble(measurementInput.getText().toString());
+                    Unit fromUnit = Unit.valueOf(position1.toUpperCase());
+                    Unit toUnit = Unit.valueOf(position2.toUpperCase());
 
-                double input = Double.parseDouble(measurement.getText().toString());
-                Unit fromUnit = Unit.valueOf(position1.toUpperCase());
-                Unit toUnit = Unit.valueOf(position2.toUpperCase());
-
-                Converter converter = new Converter(fromUnit,toUnit);
-                double result = converter.convert(input);
-                textView.setText(String.valueOf(result));
-
+                    Converter converter = new Converter(fromUnit, toUnit);
+                    double result = converter.convert(input);
+                    textView.setText(String.valueOf(result));
+                    from_convert.setText(position2);
+                    to_convert.setText(position1);
+                }
             }
         });
 
-        sbutton.setOnClickListener(new View.OnClickListener() {
+        settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSettings();
@@ -56,19 +78,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(POS1, position1);
+        outState.putString(POS2, position2);
+    }
+
+    public void openSettings(){
+        Intent intent = new Intent(this, Settings.class);
+        startActivityForResult(intent, Settings.SETTINGS_REQUEST);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == Settings.SETTINGS_REQUEST) {
             if (resultCode == RESULT_OK) {
+                assert data != null;
                 position1 = data.getStringExtra("position1");
                 position2 = data.getStringExtra("position2");
             }
         }
-    }
-
-    public void openSettings(){
-        Intent intent = new Intent(this, Settings.class);
-        startActivity(intent);
     }
 }
